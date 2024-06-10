8db676d5a4fbf2e135aa104c5b7ad7dd13b65577
Julya Shapaeva <shapaeva03@mail.ru>
2024-06-10 22:27:28 +0300

    Fix problems in fill_catalog command

--- catalog/management/commands/fill_catalog.py
+++ catalog/management/commands/fill_catalog.py
@@ -12,10 +12,13 @@ class Command(BaseCommand):
     def json_read_categories():
         """Метод для получения данных о категориях с JSON-файла"""
 
-        with open(os.path.join('..', '..', '..', 'catalog.json')) as jsonfile:
-            content = json.loads(jsonfile)
+        with open('catalog.json') as jsonfile:
+            content = json.load(jsonfile)
 
-        categories = [note for note in content if note['model'] == 'catalog.category']
+        categories = []
+        for note in content:
+            if note['model'] == 'catalog.category':
+                categories.append({'pk': note['pk'], 'fields': note['fields']})
 
         return categories
 
@@ -23,10 +26,13 @@ class Command(BaseCommand):
     def json_read_products():
         """Метод для получения данных о продуктах с JSON-файла"""
 
-        with open(os.path.join('..', '..', '..', 'catalog.json')) as jsonfile:
-            content = json.loads(jsonfile)
+        with open('catalog.json') as jsonfile:
+            content = json.load(jsonfile)
 
-        products = [note for note in content if note['model'] == 'catalog.product']
+        products = []
+        for note in content:
+            if note['model'] == 'catalog.product':
+                products.append({'pk': note['pk'], 'fields': note['fields']})
 
         return products
 
@@ -41,16 +47,19 @@ class Command(BaseCommand):
 
         for category in Command.json_read_categories():
             category_for_create.append(
-                Category(name=category['name'], description=category['description'])
+                Category(pk=category['pk'], name=category['fields']['name'],
+                         description=category['fields']['description'])
             )
 
         Category.objects.bulk_create(category_for_create)
 
         for product in Command.json_read_products():
             product_for_create.append(
-                Product(name=product['name'], description=product['description'], preview=product['preview'],
-                        category=Category.objects.get(pk=product['category_id']), price=product['price'],
-                        created_at=product['created_at'], updated_at=product['updated_at'])
+                Product(pk=product['pk'], name=product['fields']['name'], description=product['fields']['description'],
+                        preview=product['fields']['preview'],
+                        category=Category.objects.get(pk=product['fields']['category']),
+                        price=product['fields']['price'], created_at=product['fields']['created_at'],
+                        updated_at=product['fields']['updated_at'])
             )
 
         Product.objects.bulk_create(product_for_create)
