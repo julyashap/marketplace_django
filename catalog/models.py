6cb617c6bfa4842f161f6d457eccf8baf2f43913
Julya Shapaeva <shapaeva03@mail.ru>
2024-06-10 17:55:57 +0300

    Update models with fields

--- catalog/models.py
+++ catalog/models.py
@@ -1,23 +1,35 @@
 from django.db import models
 
-
-class Product(models.Model):
-    """Модель для данных о продаваемых продуктах"""
-
-    def __str__(self):
-        pass
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
+NULLABLE = {'null': True, 'blank': True}
 
 
 class Category(models.Model):
     """Модель для данных о представленных категориях"""
 
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+
     def __str__(self):
-        pass
+        return f"{self.pk}. {self.name}"
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    """Модель для данных о продаваемых продуктах"""
+
+    name = models.CharField(max_length=100, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='products/', verbose_name='картинка', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория')
+    created_at = models.DateField(verbose_name='дата создания')
+    updated_at = models.DateField(verbose_name='дата последнего изменения')
+
+    def __str__(self):
+        return f"{self.pk}. {self.name}"
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
