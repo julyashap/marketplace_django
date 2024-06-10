ca41d4b95da6556272aae4e70b61307689fbf901
Julya Shapaeva <shapaeva03@mail.ru>
2024-06-10 22:53:56 +0300

    Update controller for contacts page with Contact model

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from catalog.models import Product
+from catalog.models import Product, Contact
 
 
 def home(request):
@@ -11,9 +11,12 @@ def home(request):
 
 
 def contacts(request):
+    contact = Contact.objects.get(pk=1)
+
     if request.method == 'POST':
         name = request.POST.get('name')
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'{name} ({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+
+    return render(request, 'catalog/contacts.html', {'contact': contact})
