4e42966ed636e747bec7986b672fed48bc0d1057
Julya Shapaeva <shapaeva03@mail.ru>
2024-06-23 13:06:13 +0300

    Refactor generics methods in CBVs

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, TemplateView, CreateView, UpdateView, DeleteView
+from pytils.translit import slugify
 
 from catalog.models import Product, Blog
 
@@ -28,9 +29,22 @@ class ProductDetailView(DetailView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.count_views += 1
+        self.object.save()
+
+        return self.object
 
 
 class BlogCreateView(CreateView):
@@ -38,11 +52,20 @@ class BlogCreateView(CreateView):
     fields = ('title', 'body', 'preview', 'created_at', 'is_published',)
     success_url = reverse_lazy('catalog:blog_list')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            blog = form.save()
+            blog.slug = slugify(blog.title)
+            blog.save()
+        return super().form_valid(form)
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'body', 'preview', 'created_at', 'is_published',)
-    success_url = reverse_lazy('catalog:blog_list')
+
+    def get_success_url(self):
+        return reverse('catalog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
