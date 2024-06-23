9dce74b0c71abf344c418a11eabec5849cc9f552
Julya Shapaeva <shapaeva03@mail.ru>
2024-06-23 12:14:41 +0300

    Add urlpatterns for Blog's CBVs

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-from catalog.views import ProductListView, ProductDetailView, ContactTemplateView
+from catalog.views import ProductListView, ProductDetailView, ContactTemplateView, BlogListView, BlogDetailView, \
+    BlogCreateView, BlogUpdateView, BlogDeleteView
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='home'),
+    path('', ProductListView.as_view(), name='product_list'),
     path('contacts/', ContactTemplateView.as_view(), name='contacts'),
-    path('home/<int:pk>/', ProductDetailView.as_view(), name='product_item')
+    path('home/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('blog/', BlogListView.as_view(), name='blog_list'),
+    path('blog/<int:pk>', BlogDetailView.as_view(), name='blog_detail'),
+    path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
+    path('blog/edit/<int:pk>', BlogUpdateView.as_view(), name='update_blog'),
+    path('blog/delete/<int:pk>', BlogDeleteView.as_view(), name='delete_blog'),
 ]
