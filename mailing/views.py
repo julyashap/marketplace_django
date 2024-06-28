55bb8a17f80b52e4ac4a293a6e08e0b64a1cf49b
Julya Shapaeva <shapaeva03@mail.ru>
2024-06-28 13:06:48 +0300

    Add CRUD views for Client, Message and Newsletter

--- mailing/views.py
+++ mailing/views.py
@@ -1,3 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView, TemplateView
+from mailing.models import Client, Message, Newsletter
 
-# Create your views here.
+
+class MailingView(TemplateView):
+    template_name = 'mailing_start.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['client'] = get_object_or_404(Client, pk=1)
+        context['message'] = get_object_or_404(Message, pk=1)
+
+        return context
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('email', 'first_name', 'last_name', 'patronymic', 'comment',)
+    success_url = '...'
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ('email', 'first_name', 'last_name', 'patronymic', 'comment',)
+    success_url = '...'
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    template_name = 'confirm_delete'
+    success_url = '...'
+
+
+class MessageCreateView(CreateView):
+    model = Message
+    fields = ('title', 'body',)
+    success_url = '...'
+
+
+class MessageListView(ListView):
+    model = Message
+
+
+class MessageUpdateView(UpdateView):
+    model = Message
+    fields = ('title', 'body',)
+    success_url = '...'
+
+
+class MessageDeleteView(DeleteView):
+    model = Message
+    template_name = 'confirm_delete'
+    success_url = '...'
+
+
+class NewsletterCreateView(CreateView):
+    model = Newsletter
+    fields = ('first_sending', 'periodicity', 'client', 'message',)
+    success_url = '...'
+
+
+class NewsletterListView(ListView):
+    model = Newsletter
+
+
+class NewsletterUpdateView(UpdateView):
+    model = Newsletter
+    fields = ('first_sending', 'periodicity', 'client', 'message',)
+    success_url = '...'
+
+
+class NewsletterDeleteView(DeleteView):
+    model = Newsletter
+    template_name = 'confirm_delete'
+    success_url = '...'
