1809dfe60b671e88f900e56c1a0bc6ce146b9ea4
Julya Shapaeva <shapaeva03@mail.ru>
2024-06-28 19:04:08 +0300

    Add models to admin

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing.models import Client, Message, Newsletter, NewsletterLog
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_name', 'last_name', 'patronymic', 'email',)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'body',)
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'first_sending', 'periodicity', 'status', 'client', 'message',)
+
+
+@admin.register(NewsletterLog)
+class NewsletterLogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'last_attempt', 'status', 'server_answer',)
