031bdbf71b898ace3efd8610bdc20549024c1908
Julya Shapaeva <shapaeva03@mail.ru>
2024-07-14 21:34:24 +0400

    Create command for create superuser

---
+++ users/management/commands/csu.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+from users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        superuser = User.objects.create(
+            email='shapaeva03@mail.ru',
+            first_name='Julya',
+            last_name='Shapaeva',
+            is_staff=True,
+            is_superuser=True
+        )
+
+        superuser.set_password('mila8625')
+        superuser.save()
