cd179c59de783667004ca7040a7a02f1582ff00f
Julya Shapaeva <shapaeva03@mail.ru>
2024-07-14 21:34:33 +0400

    Create model User

--- users/models.py
+++ users/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    avatar = models.ImageField(verbose_name='аватар')
+    phone = models.CharField(max_length=50, verbose_name='номер')
+    country = models.CharField(max_length=100, verbose_name='страна')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
